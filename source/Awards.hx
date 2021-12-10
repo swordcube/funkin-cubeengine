6ea667d9556fe272487f58f1cbd545f166e7401f
swordcube <49110074+swordcube@users.noreply.github.com>
2021-12-10 01:40:02 -0600

    awards menu almost fully done
    all that's left to do is the icons

--- source/Awards.hx
+++ source/Awards.hx
@@ -28,32 +28,35 @@ class Awards
 		false,
 		false,
 		false,
-		false
+		false,
+		false,
 	];
 	
 	// -- THE SHIT --
 	public static var awardsStuff:Array<Dynamic> = [
-		["Freaky on a Friday Night", 	"Play the game on a friday night.", 						"FridayNightPlay", 	"FridayNightPlay", 	"/misc", 			false],
-		["Beginners Luck", 				"Beat Week 1 with no misses.", 								"Week1", 			"Week1", 			"/weeks", 			false],
-		["Trick or Treat", 				"Beat Week 2 with no misses.", 								"Week2", 			"Week2", 			"/weeks", 			false],
-		["Bullet Dodger", 				"Beat Week 3 with no misses.", 								"Week3", 			"Week3", 			"/weeks", 			false],
-		["Lady Killer", 				"Beat Week 4 with no misses.", 								"Week4", 			"Week4", 			"/weeks", 			false],
-		["Christmas Morning", 			"Beat Week 5 with no misses.", 								"Week5", 			"Week5", 			"/weeks", 			false],
-		["Highscore", 					"Beat Week 6 with no misses.", 								"Week6", 			"Week6", 			"/weeks", 			false],	
-		["Get Good", 					"Beat a song with accuracy lower than 20%.", 				"GetGood", 			"GetGood", 			"/misc", 			false],
-		["Danger Zone", 				"Beat a song with health lower than 20%.", 					"DangerZone", 		"DangerZone", 		"/misc", 			false],
-		["osu!mania player", 			"Beat a song with 100% accuracy.", 							"OsuManiaPlayer", 	"OsuManiaPlayer", 	"/misc", 			false],
-		["Double Sided", 				"Beat a song by only pressing 2 of your keybinds.", 		"DoubleSided", 		"DoubleSided", 		"/misc", 			false],
-		["Debugger", 					"Beat \"Test\" from the chart editor.", 					"Debugger", 		"Debugger", 		"/misc", 			true],
-		["Master Debugger", 			"Beat \"Test\" from the chart editor and get no misses.", 	"MasterDebugger", 	"MasterDebugger", 	"/misc", 			true],
-		["One small issue...", 			"127.0.0.1\ni have ur ip adress be scared", 				"IPAddress", 		"IPAddress", 		"/misc", 			true]
+		["Freaky on a Friday Night", 	"Play the game on a friday night.", 						"FridayNightPlay", 	"FridayNightPlay", 	"/misc", 			true, 0xFFeb9e34],
+		["Beginners Luck", 				"Beat Week 1 with no misses.", 								"Week1", 			"Week1", 			"/weeks", 			false, 0xFFaf66ce],
+		["Trick or Treat", 				"Beat Week 2 with no misses.", 								"Week2", 			"Week2", 			"/weeks", 			false, 0xFFd57e00],
+		["Bullet Dodger", 				"Beat Week 3 with no misses.", 								"Week3", 			"Week3", 			"/weeks", 			false, 0xFFb7d855],
+		["Lady Killer", 				"Beat Week 4 with no misses.", 								"Week4", 			"Week4", 			"/weeks", 			false, 0xFFd8558e],
+		["Christmas Morning", 			"Beat Week 5 with no misses.", 								"Week5", 			"Week5", 			"/weeks", 			false, 0xFFaf66ce],
+		["Highscore", 					"Beat Week 6 with no misses.", 								"Week6", 			"Week6", 			"/weeks", 			false, 0xFFeb9e34],	
+		["Get Good", 					"Beat a song with accuracy lower than 20%.", 				"GetGood", 			"GetGood", 			"/misc", 			false, 0xFFeb9e34],
+		["Danger Zone", 				"Beat a song with health lower than 20%.", 					"DangerZone", 		"DangerZone", 		"/misc", 			false, 0xFFeb9e34],
+		["osu!mania player", 			"Beat a song with 100% accuracy.", 							"OsuManiaPlayer", 	"OsuManiaPlayer", 	"/misc", 			false, 0xFFeb9e34],
+		["Double Sided", 				"Beat a song by only pressing 2 of your keybinds.", 		"DoubleSided", 		"DoubleSided", 		"/misc", 			false, 0xFFeb9e34],
+		["Debugger", 					"Beat \"Test\" from the chart editor.", 					"Debugger", 		"Debugger", 		"/misc", 			true, 0xFFeb9e34],
+		["Master Debugger", 			"Beat \"Test\" from the chart editor and get no misses.", 	"MasterDebugger", 	"MasterDebugger", 	"/misc", 			true, 0xFFeb9e34],
 		#if windows
-		,["Bloatware", 					"Why does Windows come with so much bloatware??", 			"toasterWindows", 	"Toaster", 			"/misc/toaster", 	false]
+		["Bloatware", 					"Either you have a shit load of startup programs\nOr it's the bloatware that's slowing you down.", 			"toasterWindows", 	"Toaster", 			"/misc/toaster", 	false, 0xFFe33b3b],
 		#elseif mac
-		,["Money Waster", 				"Is this a 2009 MacBook from EBay?!", 						"toasterMac", 		"Toaster", 			"/misc/toaster", 	false]
+		["Money Waster", 				"Is this a 2009 MacBook from EBay?!", 						"toasterMac", 		"Toaster", 			"/misc/toaster", 	false, 0xFFb2b4b8],
 		#elseif linux 
-		,["Stop Breaking Your System", 	"How do you break your system this bad so fast?!", 			"toasterLinux", 	"Toaster", 			"/misc/toaster", 	false]
+		["2009 Laptop", 	"You have to be running Linux on a 2009 laptop\nto get to this point.", 			"toasterLinux", 	"Toaster", 			"/misc/toaster", 	false, 0xFF1b1c1c],
+		#else
+		["Toaster PC", 	"Please...\nPlease just get a new PC...", 			"toasterGeneric", 	"Toaster", 			"/misc/toaster", 	false, 0xFFb2b4b8],
 		#end
+		["One small issue...", 			"127.0.0.1\ni have ur ip adress be scared", 				"IPAddress", 		"IPAddress", 		"/misc", 			true, 0xFFeb9e34],
 	];
 
 	public static function loadAwards()
--- source/AwardsState.hx
+++ source/AwardsState.hx
@@ -29,12 +29,15 @@ class AwardsState extends MusicBeatState
 	public static var menuBG:FlxSprite;
 	public static var descText:FlxText;
 	public static var descBox:FlxSprite;
+	public static var intendedColor:Int;
+	public static var colorTween:FlxTween;
 	private var checkboxGroup:FlxTypedGroup<AttachedAchievement>;
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private var checkboxNumber:Array<Int> = [];
 	private var checkboxArray:Array<AttachedAchievement> = [];
 	var daValue:Bool = false;
 	var daDesc:String = '';
+	var newColor:Int = 0;
 	
 	// the options for da menu
 	var options:Array<Dynamic> = [];
@@ -47,7 +50,7 @@ class AwardsState extends MusicBeatState
 		Awards.loadAwards();
 		
 		// menu bg
-		menuBG = new FlxSprite().loadGraphic(Paths.image('menuBGBlue'));
+		menuBG = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
 		menuBG.updateHitbox();
 		menuBG.screenCenter();
@@ -64,23 +67,25 @@ class AwardsState extends MusicBeatState
 		
 		for (i in 0...Awards.awardsStuff.length)
 		{
-			var optionText:Alphabet = new Alphabet(0, 70 * i, Awards.awardsStuff[i][0], false, false, null);
-			optionText.isMenuItem = true;
-			optionText.x += 300;
-			/*optionText.forceX = 300;
-			optionText.yMult = 90;*/
-			optionText.xAdd = 200;
-			optionText.targetY = i;
-			grpOptions.add(optionText);
-			
-			//reloadValues();
-			
-			var checkbox:AttachedAchievement = new AttachedAchievement(optionText.x - 105, optionText.y, false, Awards.awardsStuff[i][4], Awards.awardsStuff[i][2]);
-			checkbox.sprTracker = optionText;
-			checkboxNumber.push(i);
-			checkboxArray.push(checkbox);
-			checkbox.ID = i;
-			checkboxGroup.add(checkbox);
+			if(Awards.awardsStuff[i][5] == false || Awards.awardsStuff[i][5] == true && Awards.awardsUnlocked[i] == true) {
+				var optionText:Alphabet = new Alphabet(0, 70 * i, Awards.awardsStuff[i][0], false, false, null);
+				optionText.isMenuItem = true;
+				optionText.x += 300;
+				/*optionText.forceX = 300;
+				optionText.yMult = 90;*/
+				optionText.xAdd = 200;
+				optionText.targetY = i;
+				grpOptions.add(optionText);
+				
+				//reloadValues();
+				
+				var checkbox:AttachedAchievement = new AttachedAchievement(optionText.x - 105, optionText.y, false, Awards.awardsStuff[i][4], Awards.awardsStuff[i][2]);
+				checkbox.sprTracker = optionText;
+				checkboxNumber.push(i);
+				checkboxArray.push(checkbox);
+				checkbox.ID = i;
+				checkboxGroup.add(checkbox);
+			}
 		}
 
 		descBox = new FlxSprite().makeGraphic(1, 1, FlxColor.BLACK);
@@ -99,6 +104,9 @@ class AwardsState extends MusicBeatState
 		descText.borderSize = 2.4;
 		add(descText);
 		
+		menuBG.color = 0xFF2e2f30;
+		intendedColor = menuBG.color;
+		
 		changeSelection(1);
 		
 		super.create();
@@ -106,11 +114,16 @@ class AwardsState extends MusicBeatState
 
 	var nextAccept:Int = 5;
 	var holdTime:Float = 0;
+	var allowAwardsCheat:Bool = true;
+	
 	override function update(elapsed:Float)
 	{
 		menuBG.antialiasing = GamePrefs.antialiasing;
 		
 		if (controls.BACK) {
+			if(colorTween != null) {
+				colorTween.cancel();
+			}
 			FlxG.switchState(new MainMenuState());
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 		}
@@ -121,6 +134,15 @@ class AwardsState extends MusicBeatState
 		
 		if (controls.UI_DOWN_P) {
 			changeSelection(1);
+		}
+		
+		if (FlxG.keys.justPressed.SPACE && allowAwardsCheat) {
+			if(Awards.awardsUnlocked[checkboxNumber[curSelected]] == true) {
+				Awards.awardsUnlocked[checkboxNumber[curSelected]] = false;
+			} else {
+				Awards.awardsUnlocked[checkboxNumber[curSelected]] = true;
+			}
+			changeSelection();
 		}
 		
 		descText.text = daDesc;
@@ -137,17 +159,34 @@ class AwardsState extends MusicBeatState
 		super.update(elapsed);
 	}
 	
-	function changeSelection(change:Int)
+	function changeSelection(change:Int = 0)
 	{
 		curSelected += change;
 		
 		if(curSelected < 0)
-			curSelected = Awards.awardsStuff.length - 1;
+			curSelected = grpOptions.members.length - 1;
 			
-		if(curSelected > Awards.awardsStuff.length - 1)
+		if(curSelected > grpOptions.members.length - 1)
 			curSelected = 0;
 			
-		daDesc = Awards.awardsStuff[curSelected][1];
+		if(Awards.awardsUnlocked[checkboxNumber[curSelected]] == true) {
+			newColor = getCurrentBGColor();
+		} else {
+			newColor = 0xFF2e2f30;
+		}
+		if(newColor != intendedColor) {
+			if(colorTween != null) {
+				colorTween.cancel();
+			}
+			intendedColor = newColor;
+			colorTween = FlxTween.color(menuBG, 1, menuBG.color, intendedColor, {
+				onComplete: function(twn:FlxTween) {
+					colorTween = null;
+				}
+			});
+		}
+			
+		daDesc = Awards.awardsStuff[checkboxNumber[curSelected]][1];
 		
 		var bullShit:Int = 0;
 
@@ -173,5 +212,10 @@ class AwardsState extends MusicBeatState
 				checkboxGroup.members[i].unlocked = daValue;
 			}
 		}
+	}
+	
+	function getCurrentBGColor() {
+		var bgColor:Int = Awards.awardsStuff[checkboxNumber[curSelected]][6];
+		return /*Std.parseInt(*/bgColor/*)*/;
 	}
 }
